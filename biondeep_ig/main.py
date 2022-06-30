10275a3e03320dfa457e0c169e0db00300ca9a64
Amel_Sellami <a.sellami@instadeep.com>
2022-06-30 08:49:15 +0000

    Resolve "[IG framework] Ensembling methods single and multi experiments"

--- biondeep_ig/main.py
+++ biondeep_ig/main.py
@@ -4,6 +4,8 @@ import click
 from biondeep_ig.buckets import pull
 from biondeep_ig.buckets import push
 from biondeep_ig.compute_metrics import compute_metrics
+from biondeep_ig.ensemble import ensemblexprs
+from biondeep_ig.ensemble import ensoneexp
 from biondeep_ig.feature_selection import featureselection
 from biondeep_ig.inference import inference
 from biondeep_ig.modular import modulartrain
@@ -26,6 +28,8 @@ main.add_command(compute_metrics)
 main.add_command(inference)
 main.add_command(push)
 main.add_command(pull)
+main.add_command(ensemblexprs)
+main.add_command(ensoneexp)
 
 if __name__ == "__main__":
     main()
